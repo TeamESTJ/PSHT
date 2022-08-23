f186cefc54e4defe08429799d656d50e7ba3ad71
jay <dj.min43@gmail.com>
2022-08-23 21:23:49 +0900

    try header

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
+import Marquee from "react-fast-marquee";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Marquee speed={200} gradient={false}>
+        <h1>제보를 기다리고 있습니다</h1>
+      </Marquee>
+    </div>
+  );
 };
 
 export default Home;
