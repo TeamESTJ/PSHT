245e8771091f2e6a2eb2f2a341d87e6c23b44c0f
jay <dj.min43@gmail.com>
2022-08-27 10:35:32 +0900

    trying different settings

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -11,11 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PSHT" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="manifest" href="manifest.json" />
       </Head>
       <Component {...pageProps} />
     </>
---
+++ front-end/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-appstore.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PSHT" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
