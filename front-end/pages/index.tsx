a37c7710783807ed919c20df04c4856a38c5fb0f
jay <dj.min43@gmail.com>
2022-08-28 19:23:16 +0900

    add button

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from "next";
 import Marquee from "react-fast-marquee";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <Marquee speed={200} gradient={false}>
-        <h1>제보를 기다리고 있습니다</h1>
+        <h1>제보를 기다리고 있습니다!</h1>
       </Marquee>
+      <p>제보 {count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>제보하기</button>
     </div>
   );
 };
