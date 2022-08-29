71c9eb09ea63327201b1d5abc77213976ff628b2
Jay Min <dj.min43@gmail.com>
2022-08-29 16:30:43 +0900

    rough websocket

--- mobile-application/App.tsx
+++ mobile-application/App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
-import { WebView } from "react-native-webview";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { WebView, } from "react-native-webview";
 
 const PSHT = "https://psht-djmin43.vercel.app/";
+const GTO = "https://gitm.or.kr"
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <View style={{ width: "100%", height: "100%" }}>
-        <WebView source={{ uri: PSHT }} />
+        <WebView source={{ uri: GTO }} />
       </View>
     </SafeAreaView>
   );
